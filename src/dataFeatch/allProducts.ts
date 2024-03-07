e7648ba3dd57e9f2262b7e8a418e1415cbf6286e
MahbuburRahman013 <mahbub01304@gmail.com>
2024-03-07 22:27:31 +0600

    change link

--- src/dataFeatch/allProducts.ts
+++ src/dataFeatch/allProducts.ts
@@ -9,7 +9,7 @@ export type Product = {
 
 
 export default async function allProducts() {
-    const res = await fetch("http://localhost:5000/all-products");
+    const res = await fetch("https://sofaserver.vercel.app/all-products");
     const posts: Product[] = await res.json();
     return posts;
 
